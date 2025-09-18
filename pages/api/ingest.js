05456bfcd0a393a9688084a0bb50a82108d5aed7
ComplianceLoop <rj@complianceloop.com>
2025-09-17 20:51:40 -0400

    Update ingest.js

--- pages/api/ingest.js
+++ pages/api/ingest.js
@@ -1,1 +1,4 @@
-export { default } from '../../complianceloop-starter/api/ingest.js';
+// pages/api/ingest.js
+// Use an explicit import + default export (more reliable than re-export in some Next builds)
+import handler from '../../complianceloop-starter/api/ingest.js';
+export default handler;
