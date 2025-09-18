20132db307088eb438ceaada79377c3ea80df406
ComplianceLoop <rj@complianceloop.com>
2025-09-18 09:41:13 -0400

    Update ingest.js
    fix: import ingest handler as ESM

--- pages/api/ingest.js
+++ pages/api/ingest.js
@@ -1,4 +1,3 @@
 // pages/api/ingest.js
-// Use an explicit import + default export (more reliable than re-export in some Next builds)
 import handler from '../../complianceloop-starter/api/ingest.js';
 export default handler;
